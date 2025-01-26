e164e41089f42c76374bc9a8fc47ec41a480ed6e
LHernandez148 <Luisfernandohb33@gmail.com>
2025-01-25 21:46:08 -0500

    Add files via upload
    First coding project about simple calculations

---
+++ main.py
@@ -0,0 +1,31 @@
+Multi1 = int(input("What first number do you want to multiply?"))
+Multi2 = int(input("And the other?"))
+ResMulti = (Multi1 * Multi2)
+print("{} multiplied by {} equals {}".format(Multi1,Multi2,ResMulti))
+
+Div1 = int(input("What first number do you want to divide?"))
+Div2 = int(input("And the other?"))
+ResDiv = (Div1 / Div2)
+print("{} divided by {} equals {}".format(Div1,Div2,ResDiv))
+
+DivNR1 = int(input("What first number do you want to divide without a remainder?"))
+DivNR2 = int(input("And the other?"))
+ResDivNR = (DivNR1 // DivNR2)
+print("{} divided by {} with no remainder equals {}".format(DivNR1,DivNR2,ResDivNR))
+
+DivR1 = int(input("What first number do you want to divide with to find only the remainder?"))
+DivR2 = int(input("And the other?"))
+ResDivR = (DivR1 % DivR2)
+print("{} divided by {} with only remainder equals {}".format(DivR1,DivR2,ResDivR))
+
+Sum1 = int(input("What first number would you like to add?"))
+Sum2 = int(input("And the other?"))
+ResSum = (Sum1+Sum2)
+print("{} added to {} equals {}".format(Sum1,Sum2,ResSum))
+
+Sub1 = int(input("What first number would you like to subtract?"))
+Sub2 = int(input("And the other?"))
+ResSub = (Sub1-Sub2)
+print("{} subtracted to {} equals {}".format(Sub1,Sub2,ResSub))
+
+print("Luis Hernandez wrote this code")
