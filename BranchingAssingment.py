773a5c2f8d5d803aa6b92d15a74b17c24ccce810
LHernandez148 <Luisfernandohb33@gmail.com>
2025-02-02 19:40:29 -0500

    Add files via upload
    Branching assingment

---
+++ BranchingAssingment.py
@@ -0,0 +1,22 @@
+Hours = int(input("How many hours were used?"))
+if(Hours > 1000):
+    Total = (((Hours - 1000)*0.09259)+(1000*0.07633))
+else:Total = (Hours * 0.07633)
+print(f"Your total charge will be ${Total}")
+
+#Just went and copied it four times to have to do multiple screenshots but it is the same code
+Hours = int(input("How many hours were used?"))
+if(Hours > 1000):
+    Total = (((Hours - 1000)*0.09259)+(1000*0.07633))
+else:Total = (Hours * 0.07633)
+print(f"Your total charge will be ${Total}")
+Hours = int(input("How many hours were used?"))
+if(Hours > 1000):
+    Total = (((Hours - 1000)*0.09259)+(1000*0.07633))
+else:Total = (Hours * 0.07633)
+print(f"Your total charge will be ${Total}")
+Hours = int(input("How many hours were used?"))
+if(Hours > 1000):
+    Total = (((Hours - 1000)*0.09259)+(1000*0.07633))
+else:Total = (Hours * 0.07633)
+print(f"Your total charge will be ${Total}")
